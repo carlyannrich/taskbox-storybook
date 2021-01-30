fa9e9907f029f645014f8b203619dadcc8bc689e
Carly Richardson <63616608+carlyannrich@users.noreply.github.com>
2021-01-30 22:53:17 +0000

    data

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import Task from "./Task";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
 
 // export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
 //   const events = {
@@ -25,7 +27,7 @@ import Task from "./Task";
 //     </div>
 //   );
 // }
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -75,16 +77,43 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
+// TaskList.propTypes = {
+//   /** Checks if it's in loading state */
+//   loading: PropTypes.bool,
+//   /** The list of tasks */
+//   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+//   /** Event to change the task to pinned */
+//   onPinTask: PropTypes.func,
+//   /** Event to change the task to archived */
+//   onArchiveTask: PropTypes.func,
+// };
+// TaskList.defaultProps = {
+//   loading: false,
+// };
+
+PureTaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   /** Event to change the task to pinned */
-  onPinTask: PropTypes.func,
+  onPinTask: PropTypes.func.isRequired,
   /** Event to change the task to archived */
-  onArchiveTask: PropTypes.func,
+  onArchiveTask: PropTypes.func.isRequired,
 };
-TaskList.defaultProps = {
+
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
